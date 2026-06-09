f07883510224482e661940220b9fd273e3599651
CreativeLapse <87617575+CreativeLapse@users.noreply.github.com>
2026-06-09 00:38:55 -0400

    Improve player move handling and AI selection
    Add input validation and computer move generation in Player.getMove. Human input now uses Coordinate.parseString and loops until board.canMove approves the start/end pair (printing "Invalid move." on failure). Computer move logic enumerates all board coordinates, collects legal moves for the player's color into an ArrayList, and returns a random legal move. Also add import for ArrayList and a simple getName() accessor.

--- Player.java
+++ Player.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 class Player {
@@ -17,24 +18,55 @@ class Player {
         this.captureCount += caps;
     }
 
+    String getName() {
+        return this.name;
+    }
+
     int captureCount() {
         return this.captureCount;
     }
 
-    Coordinate[] getMove(Board board){
+    Coordinate[] getMove(Board board) {
         Scanner input = new Scanner(System.in);
         String start = "";
         String end = "";
 
-        if (!isComputer){
-            System.out.println("Enter a start coordinate: ");
-            Coordinate startCoord = parseInt(input.next());
-            System.out.println("Enter a start coordinate: ");
-            Coordinate endCoord = parseInt(input.next());
+        if (!isComputer) {
+            while (true) {
+                System.out.println("Enter a start coordinate: ");
+                Coordinate startCoord = Coordinate.parseString(input.next());
+                System.out.println("Enter a start coordinate: ");
+                Coordinate endCoord = Coordinate.parseString(input.next());
 
+                if (!board.canMove(startCoord, endCoord)) {
+                    System.out.println("Invalid move.");
+                } else {
+                    Coordinate[] pos = { startCoord, endCoord };
+                    return pos;
+                }
+            }
 
-            
+        } else {
+            ArrayList<Coordinate[]> possibleMoves = new ArrayList<>();
+            for (int i = 1; i <= 9; i++) {
+                for (char c = 'A'; c <= 'I'; c++) {
+                    Coordinate startCoord = new Coordinate(i, String.valueOf(c));
+
+                    if (board.getPiece(startCoord).equals(this.colour)) {
+                        for (int x = 1; x <= 9; x++) {
+                            for (char y = 'A'; y <= 'I'; y++) {
+                                Coordinate endCoord = new Coordinate(x, String.valueOf(y));
+
+                                if (board.canMove(startCoord, endCoord)) {
+                                    possibleMoves.add(new Coordinate[] { startCoord, endCoord });
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return possibleMoves.get((int) (Math.random() * possibleMoves.size()));
         }
-    }
 
+    }
 }

869c7f0b08c35d8cff32bb28f0c7f691afb464dc
CreativeLapse <87617575+CreativeLapse@users.noreply.github.com>
2026-06-14 00:04:22 -0400

    Add tests exercising Board, Coordinate, Player, Game
    Append a set of print-based tests to testCases.java that instantiate a Board and exercise key methods (getPiece, canMove, canMoveHorizontally, canMoveVertically, checkAndKill, cornerKill). Also add Coordinate tests (toString, getCol, getRow, parseString), Player tests (getName, getColour, captureCount) and a Game instantiation with gameOver check. These prints are intended to validate core game logic and utility methods. Note: the Board instantiation is missing a terminating semicolon and will cause a compile error until fixed.

--- testCases.java
+++ testCases.java
@@ -21,6 +21,52 @@ public class testCases {
 
         System.out.println(Arrays.deepToString(boardTest));
 
+        Board board = new Board()
+
+        //getPiece
+        System.out.println(board.getPiece(new Coordinate(3, "a")));
+        System.out.println(board.getPiece(new Coordinate(9, "a")));
+        System.out.println(board.getPiece(new Coordinate(1, "i")));
+        
+        //canMove
+        System.out.println(board.canMove(new Coordinate(9, "a"), new Coordinate(9, "c"))); 
+        System.out.println(board.canMove(new Coordinate(9, "a"), new Coordinate(5, "a"))); 
+        System.out.println(board.canMove(new Coordinate(9, "a"), new Coordinate(1, "i"))); 
+
+        //canMoveHorizontally
+        System.out.println(board.canMoveHorizontally(0, 0, 5)); 
+
+        //canMoveVertically
+        System.out.println(board.canMoveVertically(0, 0, 5)); 
+
+        //checkAndKill
+        System.out.println(board.checkAndKill(new Coordinate(9, "a"))); 
+
+        //cornerKill
+        System.out.println(board.cornerKill(new Coordinate(9, "a")));
+
+        //coordinate methods
+        Coordinate test1 = new Coordinate(5, "d");
+        System.out.println(test1.toString()); 
+        System.out.println(test1.getCol());
+        System.out.println(test1.getRow());
+        System.out.println(Coordinate.parseString("3f"));
+
+        //player methods
+        Player test2 = new Player("Test", "w", false);
+        System.out.println(test2.getName());
+        System.out.println(test2.getColour());
+        System.out.println(test2.captureCount());
+
+        //game methods
+        Player test3 = new Player("P2", "b", false);
+        Game game = new Game(test2, test3);
+        System.out.println(game.gameOver());
+
+        
+        
+
+
     }
 
     // FOR ADDTL. TESTS, USE ABOVE FORMAT
